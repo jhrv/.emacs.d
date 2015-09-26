9be415078cc39ea5373deea900394f52f46a9248
Johnny Horvi <johnny.horvi@gmail.com>
2015-09-26 18:59:10 +0200

    removed comments

--- settings/evil-setup.el
+++ settings/evil-setup.el
@@ -9,11 +9,8 @@
 (defun set-appropriate-state ()
   (if (apply 'derived-mode-p evil-modes)
       (turn-on-evil-mode)
-    ;;(set-cursor-color dotemacs-evil/emacs-cursor)
     (turn-off-evil-mode)))
 
 (add-hook 'after-change-major-mode-hook #'set-appropriate-state)
-;;(add-hook 'find-file-hook #'turn-on-evil-mode)
-
 
 (provide 'evil-setup)
