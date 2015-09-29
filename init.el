38f31c6bfd6884224c7dca7c5862921eeccbc77a
Johnny Horvi <johnny.horvi@gmail.com>
2015-09-29 22:29:17 +0200

    removed tromey and marmalade

--- init.el
+++ init.el
@@ -1,7 +1,5 @@
 ;; Define package repositories
 (require 'package)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(add-to-list 'package-archives '("tromey" . "http://tromey.com/elpa/") t)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
