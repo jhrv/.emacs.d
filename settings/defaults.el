11ecceafc1fac7aee21d2462bd9cd264098da670
Johnny Horvi <johnny.horvi@gmail.com>
2015-10-10 23:13:08 +0200

    backups fixed?

--- settings/defaults.el
+++ settings/defaults.el
@@ -18,11 +18,9 @@
 
 ;; backup files
 (defvar --backup-directory (concat user-emacs-directory "backups"))
+
 (if (not (file-exists-p --backup-directory))
         (make-directory --backup-directory t))
-(setq backup-directory-alist `(("." . ,--backup-directory)))
-(setq auto-save-file-name-transforms
-          `((".*" ,--backup-directory t)))
 
 (setq make-backup-files t               ; backup of a file the first time it is saved.
       backup-by-copying t               ; don't clobber symlinks
@@ -33,6 +31,7 @@
       kept-new-versions 9               ; newest versions to keep when a new numbered backup is made (default: 2)
       auto-save-default t               ; auto-save every buffer that visits a file
       auto-save-timeout 20              ; number of seconds idle time before auto-save (default: 30)
+      temporary-file-directory --backup-directory
       auto-save-interval 200)            ; number of keystrokes between auto-saves (default: 300)
 
 ;; go straight to scratch buffer on startup
