319315323adc835e6da80b1a44358facc3a591e4
Johnny Horvi <johnny.horvi@gmail.com>
2015-11-30 21:10:42 +0100

    added beautify-settings

--- settings/js2-setup.el
+++ settings/js2-setup.el
@@ -1,6 +1,8 @@
 (require 'js2-mode)
 
-(add-to-list 'auto-mode-alist '("\\.js$\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
+
 (setq-default js2-strict-missing-semi-warning nil)
 
 (provide 'js2-setup)
--- settings/web-beautify-setup.el
+++ settings/web-beautify-setup.el
@@ -3,13 +3,10 @@
 (eval-after-load 'js2-mode
   '(define-key js2-mode-map (kbd "C-M-l") 'web-beautify-js))
 
-(eval-after-load 'json-mode
-  '(define-key json-mode-map (kbd "C-c b") 'web-beautify-js))
-
 (eval-after-load 'sgml-mode
-  '(define-key html-mode-map (kbd "C-c b") 'web-beautify-html))
+  '(define-key html-mode-map (kbd "C-M-l") 'web-beautify-html))
 
 (eval-after-load 'css-mode
-  '(define-key css-mode-map (kbd "C-c b") 'web-beautify-css))
+  '(define-key css-mode-map (kbd "C-M-l") 'web-beautify-css))
 
 (provide 'web-beautify-setup)
